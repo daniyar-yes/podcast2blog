8195dfc7ab4673c0f46d2cab3b6124cc2adeecb6
Daniyar <yeskaliyevdan@hotmail.com>
2023-01-06 18:11:34 -0800

    correct audio link found in enclosure and cleaned via regex

--- src/App.js
+++ src/App.js
@@ -5,28 +5,36 @@ import { useState, useEffect } from 'react'
 function App() {
 
   let rssVar
+  const regex = /url="(.*?)"/;
 
-  const [rssFile, setRssFile] = useState();
-  const [audioURL, setAudioURL] = useState('')
+  const [titles, setTitles] = useState([]);
+  const [audioURLS, setAudioURLS] = useState([]);
+  const [descriptions, setDescriptions] = useState([]);
   let podTitle = document.querySelector('p')
   
 
   useEffect(() => {
     // put XML fetch logic here
-    console.log(RSS)
     const xhr = new XMLHttpRequest;
     xhr.open('GET', RSS);
     xhr.responseType = 'document';
     xhr.overrideMimeType('text/xml');
+    let links = []
 
     xhr.onload = () => {
         if (xhr.readyState === xhr.DONE && xhr.status === 200) {
-            console.log(xhr.response, xhr.responseXML);
             rssVar = xhr.responseXML;
-            podTitle = rssVar.querySelector('item title').innerHTML;
+            console.log(rssVar)
+            setTitles(rssVar.querySelectorAll('item title')[0].textContent);
             console.log(rssVar.querySelector('item title'));
-            setAudioURL(rssVar.querySelector('item link').innerHTML);
+            setAudioURLS(rssVar.querySelector('item link').innerHTML);
             console.log(rssVar.querySelectorAll('item link')[0].textContent);
+            console.log(rssVar.querySelectorAll('item link'))
+            rssVar.querySelectorAll('item enclosure').forEach((link) => links.push(link.outerHTML))
+            setAudioURLS(links)
+            const matches = rssVar.querySelectorAll('item enclosure')[0].outerHTML.match(regex)
+            setAudioURLS(matches[1])
+            console.log(rssVar.querySelector('image url').innerHTML)
         }
     };
 
@@ -36,11 +44,18 @@ function App() {
   return (
     <div className="App">
       <form>
-        <input label='rss' type='text' placeholder={`${RSS}`}></input>
+        <input 
+          label='rss' 
+          type='text' 
+          placeholder={`${RSS}`}
+        >
+        </input>
         <button type='submit'>Get RSS</button>
       </form>
+      <audio controls src={audioURLS}></audio>
       <audio controls src={sampleAudio}></audio>
       <p>#3 - IELTS Reading - советы по подготовке к секции ридинг и не только</p>
+      <h3>{titles} </h3>
     </div>
   );
 }

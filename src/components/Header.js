cf193311a838701b7b867eb24a7fea168c11eb9d
Daniyar <yeskaliyevdan@hotmail.com>
2023-04-21 08:56:44 -0700

    added link to source code to Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,8 +6,9 @@ function Header() {
     <header>
       <h2>Welcome to Podcast2Blog!</h2>
       <div className={styles.appDescription}>
-        <p>This App dynamically renders Podcast feeds from any valid Spotify for Podcasters RSS feeds (ex-Anchor FM)</p>
-        <p>By default, it renders my own podcast for example, but you can render your own podcast by putting a link in the form below</p>
+        <p>This App dynamically renders ad-free Podcast feeds from any valid Spotify for Podcasters RSS feeds (ex-Anchor FM), without the need for API keys and subscription.</p>
+        <p>By default, it renders my own podcast for example, but you can render any podcast by putting a valid RSS link in the form below.</p>
+        <p>Refer to this <a href="https://github.com/daniyar-yes/podcast2blog" target="_blank">react code</a> to implement the same approach for your Podcast's website.</p>
       </div>
     </header>
   )

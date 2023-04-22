57803c3ab9dfdf339d6acbca959e130c567d16ed
Daniyar <yeskaliyevdan@hotmail.com>
2023-04-22 00:44:59 -0700

    used useRef to prevent simulatenous audio playback

--- src/components/podcast_components/Episode.js
+++ src/components/podcast_components/Episode.js
@@ -1,13 +1,33 @@
-import styles from './Episode.module.css'
+import styles from './Episode.module.css';
+import React, { useRef, useEffect } from 'react';
 
 const Episode = ( {episode}) => {
+  const audioRef = useRef(null);
+
+  useEffect(() => {
+    const handlePlay = () => {
+      const allAudioElements = document.querySelectorAll('audio');
+      allAudioElements.forEach((audioEl) => {
+        if (audioEl !== audioRef.current) {
+          audioEl.pause();
+        }
+      });
+    };
+
+    const audioElement = audioRef.current;
+    audioElement.addEventListener('play', handlePlay);
+
+    return () => {
+      audioElement.removeEventListener('play', handlePlay)
+    };
+  }, []);
   
   return (
     <li className={styles.episode}>
         <h3>{episode.title} </h3>
         <p>{episode.description}</p>
         <p>Date Published: {episode.date}</p>
-        <audio controls src={episode.audioURL}></audio>
+        <audio ref={audioRef} controls src={episode.audioURL}></audio>
         
     </li>
   )
